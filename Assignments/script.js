096d8da23a745b1ba4e4f5b885fe88638a455e57
eeshitha-vaishnavi-03 <eeshithavaishnavi73@gmail.com>
2024-08-26 22:47:44 +0530

    Create script.js

---
+++ Assignments/script.js
@@ -0,0 +1,50 @@
+//////Variables Declarations//////
+//Create Variables
+var firstName="Eeshitha";
+console.log(firstName);
+let lastName="Vaishnavi";
+console.log(lastName);
+const age=21;
+console.log(age);
+console.log(`My name is ${firstName} ${lastName} and Iam ${age} years old`);
+
+//Variable Scope
+
+
+
+
+////Operators///
+//Arthemetic Operators
+let a = 10;
+let b = 5;
+let c = 2;
+let addition = (a + b) + c;
+let subtraction = (a - b) - c;
+let multiplication = (a * b) * c;
+let division = (a / b) / c;
+let modulus = (a % b) % c;
+console.log("Addition: " + addition); 
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+console.log("Modulus: " + modulus); 
+
+//Comparison operators
+
+
+//Logical operators
+
+
+/////Data Types/////
+//Primitive data types
+let a1 = 42;               
+let b1 = "Hello, World!";    
+let c1 = true;            
+let d1 = null;               
+let undefinedVariable;                  
+
+console.log("a1 Value =", a1, ", Type =", typeof a1);     
+console.log("b1 Value =", b1, ", Type =", typeof b1);    
+console.log("c1 Value =", c1, ", Type =", typeof c1);  
+console.log("d1 Value =", d1, ", Type =", typeof d1);          
+console.log("undefinedVariable: Value =", undefinedVariable, ", Type =", typeof undefinedVariable); 
