88121fe1143e215a47c30f2d419a0f83155d6259
eeshitha-vaishnavi-03 <eeshithavaishnavi73@gmail.com>
2024-08-26 13:43:56 +0530

    Create assignments.js

---
+++ Variable Declarations/assignments.js
@@ -0,0 +1,10 @@
+var firstName="Eeshitha";
+console.log(firstName);
+
+var lastName="Vaishnavi";
+console.log(lastName);
+
+var age=21;
+console.log(age);
+
+console.log(`My name is ${firstName} ${lastName} and Iam ${age} years old`);
