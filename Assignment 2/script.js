8870bcfabab0179bcaac140024a603c58ed6b92d
eeshitha-vaishnavi-03 <eeshithavaishnavi73@gmail.com>
2024-09-18 15:29:12 +0530

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -9,4 +9,23 @@ console.log(fruits.length);
 
 ////////////////### Operations: Add, Remove - push(), pop(), unshift(), shift() Methods in Arrays
 
+const friends=["Micheal","Steven","Peter"];
+console.log(friends);
+
+//Add Elements
+const newLength=friends.push("Eeshu","Vaishu");
+console.log(newLength);
+console.log(friends);
+
+//Remove elements
+friends.pop();
+console.log(friends);
+
+//unshift()
+friends.unshift("Kumar","Maha");
+console.log(friends);
+
+//shift()
+friends.shift();
+console.log(friends);
 
