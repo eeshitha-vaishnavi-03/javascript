0615bf634a1ee94e770a70e6ff2457561ce78fa6
eeshitha-vaishnavi-03 <eeshithavaishnavi73@gmail.com>
2024-09-18 11:51:12 +0530

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,1 +1,14 @@
-..
+//////////////////### Introduction to Arrays, Creating, Accessing - Arrays, Methods in Arrays
+
+//1. Create an array of fruits and print the elements using a for loop.
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+//2. Access the third element of an array and print it.
+console.log(fruits[2]); 
+
+//3. Find the length of an array and print it.
+console.log(fruits.length);
