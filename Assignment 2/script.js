a8fa38860b931f76fc93e90a23af796f49b3f658
eeshitha-vaishnavi-03 <eeshithavaishnavi73@gmail.com>
2024-09-20 16:00:55 +0530

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,3 +1,5 @@
+////ASSIGNMENT 2
+"use strict";
 //////////////////### Introduction to Arrays, Creating, Accessing - Arrays, Methods in Arrays
 let fruits = ["Strawberry","Avocado","Pineapple","Watermelon"];
 for (let i = 0; i < fruits.length; i++) {
@@ -28,4 +30,20 @@ console.log(friends);
 //shift()
 friends.shift();
 console.log(friends);
+
+
+////////////Introduction to Objects: Creating Objects with its Properties, Accessing Properties with dot (.) & square bracket [] representations, Appending New Properties to the Existing Objects using dot & bracket representations
+const jonasObject={
+  fullName: "Eeshitha Vaishnavi Vooda",
+  age: 2024-2003,
+  city: "Visakhapatnam"
+};
+console.log(jonasObject.fullName);
+console.log(jonasObject["fullName"]);
+
+jonasObject.Occupation="Student";
+jonasObject["Hobbies"]="Reading books, Travelling";
+
+console.log(jonasObject.Occupation);
+console.log(jonasObject["Hobbies"]);
 
